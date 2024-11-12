e211dcb959279002f173ed73e038c3518be41cdb
Anothays <jeremy.snnk@gmail.com>
2024-11-12 23:56:26 +0100

    add vite config options

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig, loadEnv } from 'vite';
+
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist-react',
-  },
-  server: {
-    port: 5123,
-    strictPort: true,
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./"),
+      }
+    },
+    base: './',
+    build: {
+      outDir: 'dist-react',
+    },
+    server: {
+      port: 5123,
+      strictPort: true,
+    }
+
+  };
 })
